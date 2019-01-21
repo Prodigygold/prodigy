d17b79b08fef92abaf9acc4b3529df1fb36bbef4
Prodiy Ayo <adebalanced04@gmail.com>
2019-01-21 18:00:39 +0100

    Sign In and Login Interface

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require ("express")
-
 const path = require("path")
+const morgan = require('morgan')
+const bodyParser = require ('body-parser')
 
-const app = express ( )
+const app = express()
 
 const PORT = 5000
+
+let userArray = [ ]
+app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, "./public")))
 
@@ -16,10 +23,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
-app.get('/', (req, res) => {
+app.get('/signIn', (req, res) => {
     res.sendFile(path.join(__dirname, './views/signIn.html'))
 })
 
+app.post('/submitForm', (req, res) => {
+    let userObject = req.body
+    userArray.push(userObject)
+    console.log(userArray)
+    res.redirect('/')
+})
 app.listen(PORT, ( ) => {
     console.log ("Hey Ya, Server is running")
 })
